246a07e177c253e52b775900f058ffe9fa4f87b8
ujaRHR <rhraju.dev@gmail.com>
2025-08-28 22:57:31 +0600

    updated: basic configs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import Router from "@koa/router";
 import bodyParser from "koa-bodyparser";
 import { connectToDb } from "./config/db.ts";
 import urlRouter from "./routes/url.route.ts";
+import authRouter from "./routes/auth.route.ts";
 
 const app = new Koa();
 const router = new Router();
@@ -12,6 +13,7 @@ app.use(bodyParser());
 await connectToDb();
 
 // Routing prefix setup
+router.use("/api/auth", authRouter.routes(), authRouter.allowedMethods());
 router.use("/api/urls", urlRouter.routes(), urlRouter.allowedMethods());
 
 app.use(router.routes()).use(router.allowedMethods());
